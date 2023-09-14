b9762f8f2dfcc05b8d284574a5a4a155fa8c1889
jhuang265 <jerryhuang265@hotmail.com>
2023-09-14 19:32:32 -0400

    fix test

--- a1/tests/q1.2.py
+++ a1/tests/q1.2.py
@@ -10,7 +10,7 @@ def test_2b_public(np, cross_validation_linear_regression):
     outputs = cross_validation_linear_regression(
         k_folds=2, hyperparameters=[0],
         X=np.asarray([[1, 0], [0, 1], [1, 1], [2, 4]], dtype=np.float64),
-        y=np.asarray([0, 0, 0, 0], dtype=np.float64),
+        y=np.asarray([[0], [0], [0], [0]], dtype=np.float64),
     )
 
     np.testing.assert_allclose(
@@ -24,4 +24,6 @@ def test_2b_public(np, cross_validation_linear_regression):
     np.testing.assert_allclose(
         outputs[2], np.asarray([0.0], dtype=np.float64), atol=1e-6
     )
+
+    print(outputs)
 
