c13f93adf9e06c9e36e38c9a66cbd86dca4a8375
Helen <helenzhangtianyue@gmail.com>
2023-10-30 13:47:23 -0400

    Update q1.1.py

--- a4/tests/q1.1.py
+++ a4/tests/q1.1.py
@@ -7,5 +7,5 @@ points = 10
 
 
 @test_case(points=1.5, hidden=False)
-def test_q1_1_public_1(np, pd, output_data_shape):
-    assert output_data_shape == (1,2), "output data shape do not match the expected value"
+def test_q1_1_public_1(np, pd, output_data):
+    assert output_data.shape == (1,2), "output data shape do not match the expected value"
